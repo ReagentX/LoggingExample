8b8639e9c1b140aa789270cc5e9e1d743fa91df3
Christopher Sardegna <gpg@reagentx.net>
2020-10-17 10:34:08 -0400

    Better handling of default configurations

--- lib/logger.py
+++ lib/logger.py
@@ -5,6 +5,12 @@ import socket
 
 from .color_formatter import ColorFormatter
 
+DEFAULT_FILES = {
+    'dev': 'logger_config/info.conf',
+    'test': 'logger_config/info.conf',
+    'prod': 'logger_config/error.conf',
+}
+
 
 def build_logger() -> logging.Logger:
     """
@@ -17,35 +23,35 @@ def build_logger() -> logging.Logger:
     # Set the color prop so we can access it from the config
     logging.ColorFormatter = ColorFormatter  # type: ignore
 
-    # Read initial config file if we are in a deployed environment
-    if os.environ.get('ENV') in {'dev', 'test', 'prod'}:
-        logging.config.fileConfig('logger_config/info.conf')
-        # Create and start listener on an open port
-        port = 9001
-        logging_config_listener = None
-        while port < 9030:
-            try:
-                # Check if the socket is in use
-                socket_validator = socket.socket(socket.AF_INET,
-                                                 socket.SOCK_STREAM)
-                socket_validator.connect(('localhost', port))
-                socket_validator.close()
-                port += 1
-                continue
-            # If the socket does not exist, Python raises this exception and we know we can use it
-            except ConnectionRefusedError:
-                logging_config_listener = logging.config.listen(port)
-                logging_config_listener.start()
-                print(f'Setup logging server on port {port}')
-                if port == 9030:
-                    print('Out of logging ports! Please increase limit!')
-                break
-        if logging_config_listener is None:
-            raise ValueError(
-                'Unable to start logging server, all ports in use!')
-    else:
-        # Use debug level logs as default for non-deployed dev
-        logging.config.fileConfig('logger_config/debug.conf')
+    # Read initial config file based on environment name, default to debug
+    logging.config.fileConfig(
+        DEFAULT_FILES.get(
+            os.environ.get('ENV', ''), 
+            'logger_config/debug.conf'))
+
+    # Create and start listener on an open port
+    port = 9001
+    logging_config_listener = None
+    while port < 9030:
+        try:
+            # Check if the socket is in use
+            socket_validator = socket.socket(socket.AF_INET,
+                                             socket.SOCK_STREAM)
+            socket_validator.connect(('localhost', port))
+            socket_validator.close()
+            port += 1
+            continue
+        # If the socket does not exist, Python raises this exception and we know we can use it
+        except ConnectionRefusedError:
+            logging_config_listener = logging.config.listen(port)
+            logging_config_listener.start()
+            print(f'Setup logging server on port {port}')
+            if port == 9030:
+                print('Out of logging ports! Please increase limit!')
+            break
+    if logging_config_listener is None:
+        raise ValueError(
+            'Unable to start logging server, all ports in use!')
     logger = logging.getLogger()  # Root logger
     return logger
 
